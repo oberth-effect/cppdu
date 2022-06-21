79f6a166d6f2f6784ff00bd54721b29a28465c19
Štěpán Venclík <stepan.venclik@gmail.com>
2022-06-21 11:45:41 +0200

    added sum testing

--- du2-faktorial/main.c
+++ du2-faktorial/main.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
-typedef unsigned CARDINAL;
-#define CPT "u" // CARDINAL Printf Type (without %)
-//typedef unsigned long long CARDINAL;
-//#define CPT "llu" // CARDINAL Printf Type (without %)
+//typedef unsigned CARDINAL;
+//#define CPT "u" // CARDINAL Printf Type (without %)
+typedef unsigned long long CARDINAL;
+#define CPT "llu" // CARDINAL Printf Type (without %)
 
 CARDINAL combination(CARDINAL n, CARDINAL k) {
     if (k > n / 2) {
@@ -17,6 +17,12 @@ CARDINAL combination(CARDINAL n, CARDINAL k) {
 }
 
 int main() {
-    printf("%"CPT"\n", combination(129, 60));
+    for (int n = 1; n < 200; n += 10) {
+        for (int k = 1; k < n; k += 10) {
+            printf("n=%d, k=%d: ", n, k);
+            printf("%"CPT" == %"CPT" %s\n", combination(n + 1, k + 1), combination(n, k + 1) + combination(n, k),
+                   combination(n + 1, k + 1) == combination(n, k + 1) + combination(n, k) ? "true" : "false");
+        }
+    }
     return 0;
 }
