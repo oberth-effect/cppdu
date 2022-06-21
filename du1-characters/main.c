d206b3b7544c38f99d2d41c7d1781d4435c50d3a
Štěpán Venclík <stepan.venclik@gmail.com>
2022-06-21 11:30:18 +0200

    fix isspace

--- du1-characters/main.c
+++ du1-characters/main.c
@@ -6,7 +6,7 @@ int main() {
     printf( " hex|dec|chr|print|control|wspace|alnum" "\n" );
     printf( "---------------------------------------" "\n" );
     for(unsigned int i=0;i<128;i++){
-        printf("0x%.2x|%3d|%3c|%5c|%7c|%6c|%5c\n",i,i,isprint(i)?i:' ', isprint(i) ? 'x':' ', iscntrl(i) ? 'x' :' ', iswspace(i) ? 'x':' ',
+        printf("0x%.2x|%3d|%3c|%5c|%7c|%6c|%5c\n",i,i,isprint(i)?i:' ', isprint(i) ? 'x':' ', iscntrl(i) ? 'x' :' ', isspace(i) ? 'x':' ',
                isalnum(i)? 'x':' ');
     }
 
